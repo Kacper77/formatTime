aaf81288cfe4f6211fb9b34ce79ceb61c46117e0
Kacper Stroisz <kacperstroisz@wp.pl>
2018-03-16 23:02:17 +0100

    Change time

--- modules/time.js
+++ modules/time.js
@@ -1,14 +1,15 @@
-function formatTime(secounds) {
-    if (secounds < 60) {
-        return secounds + ' sek.';
-    } else if (secounds < 3600) {
-        var minutes = Math.floor(secounds / 60);
-        var sec = secounds % 60;
-        return minutes + ' min.' + sec + ' sek.'
-    } else(secounds > 3600)
-    var hour = Math.floor(secounds / 3600);
-    return hour + ' hours ' + minutes + ' min. ' + sec + ' sek.'
-
-
+function formatTime(seconds) {
+    if (seconds < 60) {
+        return seconds + ' sek.';
+    } else if (seconds < 3600) {
+        var minutes = Math.floor(seconds / 60);
+        var sec = seconds % 60;
+        return minutes + ' min.' + sec + ' sek.';
+    } else {
+        var hour = Math.floor(seconds / 3600);
+        var minutes = Math.floor(seconds % 3600 / 60);
+        var sec = Math.floor(seconds % 3600 / 60);
+        return hour + ' hours ' + minutes + ' min. ' + sec + ' sek.';
+    }
 }
 exports.formatTime = formatTime;
